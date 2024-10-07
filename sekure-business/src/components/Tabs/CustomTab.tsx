6c51d28719cc4f8fbd5ec22ed6359436199aab71
codedjay672 <emmajoel6456@gmail.com>
2024-10-07 01:07:39 +0100

    fixed bug in the signup form state

--- sekure-business/src/components/Tabs/CustomTab.tsx
+++ sekure-business/src/components/Tabs/CustomTab.tsx
@@ -6,35 +6,40 @@ import { useEffect, useId, useState } from "react";
 const CustomTab: React.FC = () => {
   const pathname = usePathname();
   const id = useId();
-  const [currentTab, setCurrentTab] = useState(
-    pathname.split('/')[3] || 'informations'
-  );
-  const [prevTab, setPrevTab] = useState<string[]>([]);
+  const currentTab = pathname.split('/')[3];
+  const [prevTabs, setPrevTabs] = useState<string[]>([]);
 
-  const tabs = [
-    {id: `${id}information`, label: 'Informations' },
-    {id: `${id}adresse`, label: 'Adresse' },
-    {id: `${id}actionnaires`, label: 'Actionnaires' },
-    {id: `${id}legal`, label: 'Legal' },
-    {id: `${id}validation`, label: 'Validation' },
-  ]
+  const [tabs, setTabs] = useState([
+    { id: `${id}information`, label: 'Informations', completed: false },
+    { id: `${id}adresse`, label: 'Adresse', completed: false },
+    { id: `${id}actionnaires`, label: 'Actionnaires', completed: false },
+    { id: `${id}legal`, label: 'Legal', completed: false },
+    { id: `${id}validation`, label: 'Validation', completed: false },
+  ]);
 
   useEffect(() => {
-    tabs.map((tab) => {
-      if (pathname.includes(tab.label.toLowerCase())) {
-        setCurrentTab(tab.label.toLowerCase());
-      } 
-    })
+    const visitedTabs = tabs.slice(0, tabs.findIndex((tab) => tab.label.toLowerCase() === currentTab)).map((tab) => tab.label.toLowerCase());
+
+    const newTabs = tabs.map((tab) => {
+      if (visitedTabs.includes(tab.label.toLowerCase())) {
+        return { ...tab, completed: true };
+      }
+      return tab;
+    });
+    
+    setTabs([...newTabs]);
+
+    console.log(tabs);
   }, [pathname]);
 
   return (
     <div className="flex-between gap-2 mt-1">
-      {tabs.map((tab, idx) => (
-        <div key={tab.id} className={`relative flex-center py-[8px] w-[78.34px] border-t-4 mt-2 ${currentTab === tab.label.toLowerCase() ? 'border-t-primary text-primary': ''} text-[12px] leading-[24px] text-center`}>
-          {prevTab.map((prev) => (
-            <div className={`absolute bg-[url(/assets/icons-pack-2/success.svg)] bg-no-repeat w-[18px] h-[18px] }bg-center bg-contain -top-3 rounded-[7px]`} />
-          ))}
-          {tab.label}
+      {tabs.map((tab) => (
+        <div key={tab.id} className={`relative flex-center py-[8px] w-[78.34px] border-t-4 mt-2 ${currentTab === tab.label.toLowerCase() || tab.completed ? 'border-t-primary text-primary group': ''} text-[12px] leading-[24px] text-center`}>
+          {tab.completed && (
+            <div className={`absolute inset-0 -top-[10px] left-[30px] bg-[url(/assets/icons-pack-2/success.svg)] bg-no-repeat w-[18px] h-[18px] }bg-center bg-contain top-[4px] rounded-[7px] bg`} />
+          )}
+          <span className="group-active:text-primary">{tab.label}</span>
         </div>
       ))}
     </div>
