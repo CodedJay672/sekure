10ea343d10d7dd839366e08ffd5519fc84437563
codedjay672 <emmajoel6456@gmail.com>
2024-12-17 05:59:26 +0100

    fix error page bugs

--- sekure-business/src/app/(Home)/global-error.tsx
+++ sekure-business/src/app/(Home)/global-error.tsx
@@ -1,23 +1,18 @@
-"use client";
+"use client"; // Error boundaries must be Client Components
 
-import Link from "next/link";
-
-const ErrorPage = () => {
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   return (
-    <div className="w-full h-dvh flex justify-center items-center">
-      <div className="w-1/2 h-1/2 flex flex-col justify-center items-center">
-        <h1 className="text-5xl font-bold">Oh Snap!</h1>
-        <h2 className="text-3xl font-semibold">Something broke...</h2>
-      </div>
-
-      <Link
-        href="/"
-        className="text-pretty bg-primary-fade text-primary px-6 py-6 rounded-full"
-      >
-        Return to Home Page
-      </Link>
-    </div>
+    <html>
+      <body>
+        <h2>Oh snap! Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
   );
-};
-
-export default ErrorPage;
+}
