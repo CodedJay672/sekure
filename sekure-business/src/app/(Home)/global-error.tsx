34147ef7100b887316ec214461e8fe2b67566a36
codedjay672 <emmajoel6456@gmail.com>
2024-12-17 05:55:58 +0100

    fix error page bugs

--- sekure-business/src/app/(Home)/global-error.tsx
+++ sekure-business/src/app/(Home)/global-error.tsx
@@ -6,8 +6,8 @@ const ErrorPage = () => {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
       <div className="w-1/2 h-1/2 flex flex-col justify-center items-center">
-        <h1 className="text-5xl font-bold">404</h1>
-        <h2 className="text-3xl font-semibold">Page Not Found</h2>
+        <h1 className="text-5xl font-bold">Oh Snap!</h1>
+        <h2 className="text-3xl font-semibold">Something broke...</h2>
       </div>
 
       <Link
