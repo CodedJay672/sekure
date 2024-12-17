bb6ffa897878142cb83759a1413f8c6daca9f0e4
codedjay672 <emmajoel6456@gmail.com>
2024-12-17 06:28:56 +0100

    add loading ui

---
+++ sekure-business/src/app/(Home)/utilisateurs/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingUI from "@/components/ui/shared/Loading/LoadingUI";
+
+const loading = () => {
+  return <LoadingUI />;
+};
+
+export default loading;
---
+++ sekure-business/src/components/ui/shared/Loading/LoadingUI.tsx
@@ -0,0 +1,11 @@
+import LoadingSpinner from "@/components/Alert/Loading";
+
+const LoadingUI = () => {
+  return (
+    <div className="w-1/2 h-dvh place-content-center">
+      <LoadingSpinner />;
+    </div>
+  );
+};
+
+export default LoadingUI;
